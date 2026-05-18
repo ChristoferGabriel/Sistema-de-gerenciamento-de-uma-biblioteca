d6f4dab6297295f113a7cf81941144ba8eb6c138
chris <christofergabriel2007@gmail.com>
2026-05-18 10:40:07 -0300

    Usuario .java

---
+++ model/Usuarios.java
@@ -0,0 +1,33 @@
+public class Usuarios {
+    private String nome, email, telefone;
+
+    public Usuarios(String email, String nome, String telefone) {
+        this.email = email;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
