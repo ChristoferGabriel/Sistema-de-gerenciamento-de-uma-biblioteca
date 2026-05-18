59af3cfc995de776bdb03cf7e4b5343f5b92385c
chris <christofergabriel2007@gmail.com>
2026-05-18 10:31:12 -0300

    Pessoa .java

---
+++ model/Pessoa.java
@@ -0,0 +1,45 @@
+public class Pessoa{
+    private String nome, email, telefone, endereco;
+
+    public Pessoa(String email, String nome, String telefone, String endereco){
+        this.email = email;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
+
+
+
